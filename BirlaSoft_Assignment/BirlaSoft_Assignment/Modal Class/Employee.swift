bcd625521600a0814b4805487a217806713b7d30
Krishna Gunjal <kgunjal51@gmail.com>
2022-06-16 19:56:44 +0530

    Restructured project.

---
+++ BirlaSoft_Assignment/BirlaSoft_Assignment/Modal Class/Employee.swift
@@ -0,0 +1,8 @@
+//
+//  Employee.swift
+//  BirlaSoft_Assignment
+//
+//  Created by Krishna Gunjal on 16/06/22.
+//
+
+import Foundation
---
+++ BirlaSoft_Assignment/BirlaSoft_Assignment/View Models/ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ViewModel.swift
+//  BirlaSoft_Assignment
+//
+//  Created by Krishna Gunjal on 16/06/22.
+//
+
+import Foundation
