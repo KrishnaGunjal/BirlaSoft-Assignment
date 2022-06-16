6827496b75474334e8f732173b7a483508d4b820
Krishna Gunjal <kgunjal51@gmail.com>
2022-06-16 19:52:25 +0530

    Initial Commit.

---
+++ BirlaSoft_Assignment/BirlaSoft_Assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BirlaSoft_Assignment
+//
+//  Created by Krishna Gunjal on 16/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
